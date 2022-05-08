5924891d1320136f9675ec11266995fb0978eb58
Nicole Bidigaray <53702713+Nicole-Bidigaray@users.noreply.github.com>
2022-05-08 18:25:19 -0300

    Inserido a consulta por conta

--- src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -23,6 +23,11 @@ public class MovimentacaoController {
         return repository.findAll();
     }
 
+    @GetMapping("/{idConta}")
+    public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+        return repository.findByIdConta(idConta);
+    }
+
     @PostMapping
     public void save(@RequestBody NovaMovimentacao movimentacao) {
         service.save(movimentacao);
--- src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -3,5 +3,9 @@ package com.dio.santander.bankline.api.repository;
 import com.dio.santander.bankline.api.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+
+    public List<Movimentacao> findByIdConta(Integer idConta);
 }
